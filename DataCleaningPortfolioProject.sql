496157c67c1694cfde538298fd1e4822d5dee0f7
Akpor Hari Agboro <70113161+quixa@users.noreply.github.com>
2023-03-14 11:36:16 +0100

    Add files via upload

---
+++ DataCleaningPortfolioProject.sql
@@ -0,0 +1,146 @@
+--View all columns and rows to get familiar with the data
+SELECT*
+FROM PortfolioProject..NashvilleHousing
+
+
+--Change the SaleDate Column to date only instead of datetime
+
+SELECT SaleDate, CONVERT(date,SaleDate)
+FROM PortfolioProject..NashvilleHousing
+
+UPDATE PortfolioProject..NashvilleHousing
+SET SaleDate = CONVERT(date,SaleDate)
+
+-- This doesn't seem to work, so try something else
+
+ALTER TABLE PortfolioProject..NashvilleHousing
+ADD SaleDateConverted Date;
+
+UPDATE PortfolioProject..NashvilleHousing
+SET SaleDateConverted = CONVERT(date,SaleDate)
+
+
+--Populate PropertyAddress column
+
+SELECT a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM PortfolioProject..NashvilleHousing a
+JOIN PortfolioProject..NashvilleHousing b
+ON a.ParcelID = b.ParcelID
+AND A.[UniqueID ]<> B.[UniqueID ]
+WHERE a.PropertyAddress IS NULL
+
+
+UPDATE a
+SET 
+PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM PortfolioProject..NashvilleHousing a
+JOIN PortfolioProject..NashvilleHousing b
+ON a.ParcelID = b.ParcelID
+AND A.[UniqueID ]<> B.[UniqueID ]
+WHERE a.PropertyAddress IS NULL
+
+
+--Break out the PropertyAddress column into separate columns
+
+SELECT PropertyAddress, SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress)-1) AS Address,
+	SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress)+1, LEN(PropertyAddress))  AS City
+FROM PortfolioProject..NashvilleHousing
+
+
+ALTER TABLE PortfolioProject..NashvilleHousing
+ADD PropertySplitAddress nvarchar(255);
+
+UPDATE PortfolioProject..NashvilleHousing
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress)-1)
+
+ALTER TABLE PortfolioProject..NashvilleHousing
+ADD PropertySplitCity nvarchar(255);
+
+UPDATE PortfolioProject..NashvilleHousing
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress)+1, LEN(PropertyAddress))
+
+
+--Break out the OwnerAddress column in to separate columns using PARSENAME
+
+SELECT PARSENAME(REPLACE(OwnerAddress, ',', '.'),3), 
+PARSENAME(REPLACE(OwnerAddress, ',', '.'),2),
+PARSENAME(REPLACE(OwnerAddress, ',', '.'),1) 
+FROM PortfolioProject..NashvilleHousing
+
+
+ALTER TABLE PortfolioProject..NashvilleHousing
+ADD OwnerSplitAddress nvarchar(255);
+
+UPDATE PortfolioProject..NashvilleHousing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.'),3)
+
+ALTER TABLE PortfolioProject..NashvilleHousing
+ADD OwnerSplitCity nvarchar(255);
+
+UPDATE PortfolioProject..NashvilleHousing
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.'),2)
+
+ALTER TABLE PortfolioProject..NashvilleHousing
+ADD OwnerSplitState nvarchar(255);
+
+UPDATE PortfolioProject..NashvilleHousing
+SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.'),1) 
+
+SELECT *
+FROM PortfolioProject..NashvilleHousing
+
+
+-- Change 'Y' and 'N' to No and Yes in 'Sold as Vacant' field
+
+SELECT DISTINCT SoldAsVacant, COUNT(SoldAsVacant)
+FROM PortfolioProject..NashvilleHousing
+GROUP BY SoldAsVacant
+ORDER BY 2
+
+
+
+SELECT SoldAsVacant,
+CASE	WHEN SoldAsVacant = 'Y' THEN 'YES'
+		WHEN SoldAsVacant = 'N' THEN 'NO'
+		ELSE SoldAsVacant
+END
+FROM PortfolioProject..NashvilleHousing
+
+UPDATE PortfolioProject..NashvilleHousing
+SET SoldAsVacant = CASE	WHEN SoldAsVacant = 'Y' THEN 'YES'
+		WHEN SoldAsVacant = 'N' THEN 'NO'
+		ELSE SoldAsVacant
+END
+
+
+--Remove Duplicates
+
+WITH RowNumCTE AS(
+SELECT *, 
+	ROW_NUMBER() OVER (
+	PARTITION BY 
+	ParcelID,
+	PropertyAddress,
+	SalePrice,
+	SaleDate,
+	LegalReference
+		ORDER BY 
+			UniqueID
+			) row_num
+
+FROM PortfolioProject..NashvilleHousing
+)
+SELECT * 
+FROM RowNumCTE
+WHERE row_num >1
+ORDER BY PropertyAddress
+
+
+SELECT *
+FROM PortfolioProject..NashvilleHousing
+
+
+--Delete Unused Columns
+
+ALTER TABLE PortfolioProject..NashvilleHousing
+DROP COLUMN PropertyAddress, TaxDistrict, OwnerAddress, SaleDate
